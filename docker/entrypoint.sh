a2f8c8794fae76d2087bfcb71a1672a38610df7e
Alexis Cespedes <l22260112@matamoros.tecnm.mx>
2026-03-31 16:45:18 -0500

    add sqlite

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,26 +1,24 @@
-#Iniciar nginx
-nginx -g "daemon off;"
-
 #!/bin/bash
 
-#Iniciar PHP-FPM en background
 php-fpm &
-#Esperar un poco
 sleep 3
 
-#Generar key si no existe
+# Crear SQLite si no existe
+mkdir -p database
+touch database/database.sqlite
+
 php artisan key:generate --force
 
-#Migracion
+# Migraciones
 php artisan migrate --force
 
-#LIMPIAR ANTES
+# Limpiar cache
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
 
-# LO DE TU PROFE (SE QUEDA)
+# Cache 
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
