8a4e2ef8d280d73873be2e3c4c1f3dc85eda94a1
Alexis Cespedes <l22260112@matamoros.tecnm.mx>
2026-03-31 13:28:53 -0500

    Ajustes en pagina home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Libro;
+use App\Models\Prestamo;
+use App\Models\User;
 
 class HomeController extends Controller
 {
@@ -11,10 +13,15 @@ class HomeController extends Controller
     {
          $user = auth()->user(); // Obtener el usuario autenticado
 
-        if($user && $user->user_type === 'admin') {
+        if($user->user_type === 'admin') {
              $libros = Libro::paginate(2); // Obtener todos los libros de la base de datos con paginación
+             $total_libros = Libro::count(); // Obtener el total de libros
+             $libros_prestados = Libro::where('estatus', 1)->count(); // Obtener el total de libros prestados
+             $total_usuarios = User::count(); // Obtener el total de usuarios
+             $devoluciones_pendientes = Prestamo::where('estado', 'Pendiente')->count(); // Obtener el total de devoluciones pendientes
 
-            return view('home.index', compact('libros')); // Pasar los libros a la vista
+
+            return view('home.index', compact('libros', 'total_libros', 'libros_prestados', 'total_usuarios', 'devoluciones_pendientes')); // Pasar los libros y el total a la vista
          } else {
             return view('home.index_user');
             }
